e32b0c35018111f4b7a70d9222d1ae38ff78d576
wolfez <wolf.derechter@student.hogent.be>
2023-07-28 15:33:19 +0200

    move frontend

--- frontend/app/models/expense.js
+++
@@ -1,8 +0,0 @@
-import Model, { attr, belongsTo } from '@ember-data/model';
-
-export default class ExpenseModel extends Model {
-  @attr title;
-  @attr value;
-  @attr date;
-  @belongsTo('category', { async: true, inverse: null }) category;
-}
